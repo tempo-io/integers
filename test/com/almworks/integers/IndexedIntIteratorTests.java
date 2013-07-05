040e40417c8f30d34c7ad306dd39e09fbbe8044e
Eugene Vagin <evjava@almworks.com>
2013-07-05 10:33:36 +0000

    #11 More tests

--- test/com/almworks/integers/IndexedIntIteratorTests.java
+++ test/com/almworks/integers/IndexedIntIteratorTests.java
@@ -16,5 +16,59 @@
 
 package com.almworks.integers;
 
-public class IndexedIntIteratorTests {
+import com.almworks.integers.NativeIntFixture;
+
+import java.util.NoSuchElementException;
+
+
+public class IndexedIntIteratorTests extends NativeIntFixture{
+  public void testSimpleCase() {
+    IntArray list = IntArray.create(2, 3, 9);
+    IntIterator indexes = IntArray.create(0, 2).iterator();
+    IndexedIntIterator res = new IndexedIntIterator(list, indexes);
+    CHECK.order(res, IntArray.create(2, 9).iterator());
+
+    list = IntArray.create(1, 2, 3, 4, 5);
+    indexes = IntArray.create(0, 2, 4).iterator();
+    res = new IndexedIntIterator(list, indexes);
+    CHECK.order(res, IntArray.create(1, 3, 5).iterator());
+  }
+
+  public void testNoSuchElementExceptionCase() {
+    IntArray list = IntArray.create(2, 3, 9);
+    IntIterator indexes = IntArray.create(0, 2).iterator();
+    IndexedIntIterator res = new IndexedIntIterator(list, indexes);
+    res.next(); res.next();
+
+    // check for NSEE
+    boolean caught = false;
+    try {
+//      res.value();
+      System.out.println(res.nextValue());
+    } catch(NoSuchElementException ex) {
+      caught = true;
+    }
+    assertTrue("caught NSEE", caught);
+  }
+
+  public void testIndexOutOfBoundsExceptionCase() {
+    IntArray list = IntArray.create(2, 3, 9);
+    IntIterator indexes = IntArray.create(0, 2).iterator();
+    IndexedIntIterator res = new IndexedIntIterator(list, indexes);
+
+    // check for IOOBE
+    boolean caught = false;
+    try {
+      res.value();
+    } catch(NoSuchElementException ex) {
+      caught = true;
+    }
+    assertTrue("caught IOOBE", caught);
+  }
+  public void testRandomCase() {
+
+  }
+  public void testSpecificationCase() {
+
+  }
 }
