2c8e986866bf2005ce4b260745167a6371ee557a
Eugene Vagin <evjava@almworks.com>
2013-09-30 19:56:16 +0000

    LongUnionIteratorTwo - faster than LongUnionIterator.

---
+++ dev/com/almworks/integers/util/LongUnionIteratorTwo.java
@@ -0,0 +1,51 @@
+package com.almworks.integers.util;
+
+import com.almworks.integers.LongIterable;
+import com.almworks.integers.LongIterator;
+
+public class LongUnionIteratorTwo extends FindingLongIterator {
+  private final LongIterator myIts[] = new LongIterator[2];
+  private int myItsCount;
+
+  public LongUnionIteratorTwo(LongIterator first, LongIterator second) {
+    if (first.hasNext()) myIts[myItsCount++] = first;
+    if (second.hasNext()) myIts[myItsCount++] = second;
+    for (int i = 0; i < myItsCount; i++) {
+      switchIterator(i);
+    }
+  }
+
+  public static LongUnionIteratorTwo create(LongIterable include, LongIterable exclude) {
+    return new LongUnionIteratorTwo(include.iterator(), exclude.iterator());
+  }
+
+  protected boolean findNext() {
+    if (myItsCount == 0) return false;
+    if (myItsCount == 2) {
+      long vi = myIts[0].value();
+      long vj = myIts[1].value();
+      if (vi <= vj) {
+        myCurrent = vi;
+        switchIterator(0);
+        if (vi == vj) switchIterator(1);
+      } else {
+        myCurrent = vj;
+        switchIterator(1);
+      }
+      if (myIts[0] == null) myIts[0] = myIts[1];
+      return true;
+    }
+    myCurrent = myIts[0].value();
+    switchIterator(0);
+    return true;
+  }
+
+  private void switchIterator(int ind) {
+    if (myIts[ind].hasNext()) {
+      myIts[ind].next();
+    } else {
+      myIts[ind] = null;
+      myItsCount--;
+    }
+  }
+}
